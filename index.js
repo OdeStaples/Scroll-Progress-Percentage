dca5cf206b51761cd4309e28eb99c5d84e0ea92d
prabhuprasad95 <77141005+OdeStaples@users.noreply.github.com>
2024-07-27 16:55:22 +0530

    - added documentation for asyncScheduler

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ const scrollEvent$ = fromEvent(document, "scroll");
 const progress$ = scrollEvent$
   // parameter destructuring - passing {target} is same as passing event
   .pipe(
-    throttleTime(30, asyncScheduler, { leading: false, trailing: true }), // limits the call to calculateScrollPercentage, calculates the progress pecent atmost every 30ms
+    throttleTime(30, asyncScheduler, { leading: false, trailing: true }),
+    // limits the call to calculateScrollPercentage, calculates the progress pecent atmost every 30ms
+
+    // the issue with this approach is, the scroll percentage is calculated every 30ms (it ignores the scroll progress for the next 30ms) - hence the scroll percentage won't reflect the actual scroll progress until we start scrolling again
+
+    // asyncScheduler config properties - leading and trailing correspond with when the values should be emitted. the default behaviour is to emit at the leading edge of the silence window and then ignore all values until the window has passed.
+
+    // in case of scroll, we need to emit the value at the trailing edge - means the last value at the end of the silence window. this will give us the latest scroll position after each silence window has passed.
     map(({ target }) => calculateScrollPercentage(target.documentElement))
   )
   .subscribe((percentage) => (progressBar.style.width = `${percentage}%`));
